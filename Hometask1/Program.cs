0346b889315c37986cb8a0fada929ad01b10cc68
USER <nahidbm@code.edu.az>
2024-02-20 18:58:31 +0400

    Add project files.

---
+++ Hometask1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Hometask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            int b = 345;
+            int c = 1111;
+            int x = 1111;
+            int y = 12345;
+            int z = 56789;
+            int m = 123456;
+
+            Console.WriteLine(((Convert.ToInt32((a + b + (c * x)) + "7")+y+z)-(Convert.ToInt32(a*b + "1"))+m-(a+b+c+x))*0.18*0.03*0.01 + (y+z));
+
+        }
+    }
+}
